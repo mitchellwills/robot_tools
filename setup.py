51f86d574b6f6126f67fee40884d00101775c4af
Mitchell Wills <mwills@wpi.edu>
2015-01-05 19:39:47 -0500

    Don't try to instal nonexistent package

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['robot_tools'],
     scripts=['robot_tools.py'],
 )
 
